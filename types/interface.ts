5d17def1c84b405c862ef15de2688a3d27ebc3fe
samiulSozib <samiulcse2018@gmail.com>
2025-10-15 18:41:14 +0600

    update

--- types/interface.ts
+++ types/interface.ts
@@ -540,8 +540,8 @@ export interface Hawala {
   transaction_id?: string | number | null;
   bundle?: Bundle | null;
 
-  created_at?: Date | string;
-  updated_at?: Date | string;
+  created_at?: Date | string|null;
+  updated_at?: Date | string|null;
 }
 
 
